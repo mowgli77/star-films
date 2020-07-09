9fae33e0c0bc3b140deb7bf85765010c65c7f4f0
mowgli77 <bartend.it13@gmail.com>
2020-07-09 16:42:32 +0300

    Updates

---
+++ precache-manifest.d3a9a4ad22bbcc156d3e871240764c3b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c3bcc787eb90bcd4cb4ad894f3feb7",
+    "url": "/star-films/index.html"
+  },
+  {
+    "revision": "09651472e3af6167e16e",
+    "url": "/star-films/static/css/main.141910c0.chunk.css"
+  },
+  {
+    "revision": "c2305063f95dc37fa1be",
+    "url": "/star-films/static/js/2.b6f0e86e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-films/static/js/2.b6f0e86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09651472e3af6167e16e",
+    "url": "/star-films/static/js/main.9924812b.chunk.js"
+  },
+  {
+    "revision": "1dc8bbe770c5cb954ce1",
+    "url": "/star-films/static/js/runtime-main.47f2d82b.js"
+  },
+  {
+    "revision": "4c86c3df48e43c25b542db0fd4e3e6dd",
+    "url": "/star-films/static/media/episode1.4c86c3df.jpg"
+  },
+  {
+    "revision": "74375f3f5f2a1984cd6c6f9c1e64ae1e",
+    "url": "/star-films/static/media/episode2.74375f3f.jpg"
+  },
+  {
+    "revision": "5256d4c3bf6fcf193bd84697d8baee2f",
+    "url": "/star-films/static/media/episode3.5256d4c3.jpg"
+  },
+  {
+    "revision": "962906945366d3c09f56acf24a5722a7",
+    "url": "/star-films/static/media/episode4.96290694.jpg"
+  },
+  {
+    "revision": "4fd8a9ff68fa5450f9201c72e1adcf9e",
+    "url": "/star-films/static/media/episode5.4fd8a9ff.jpg"
+  },
+  {
+    "revision": "eea133cb535ac139fa4e12ad74d9c1f0",
+    "url": "/star-films/static/media/episode6.eea133cb.jpg"
+  }
+]);
